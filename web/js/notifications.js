689eabd8efd1042ec8ead0d2bafb55b7b3ee39c3
Harsh <harsh.kohad@gmail.com>
2018-04-12 15:44:01 +0530

    notifications & alerts changes

--- web/js/notifications.js
+++ web/js/notifications.js
@@ -6,31 +6,44 @@ $(function () {
             socket.emit('authentication', {userId: userId});
         });
         socket.on('notifications', function (rows) {
-            $('.notifications').empty();
-            $(".notifications").append("<li><p>Notifications</p></li>");
             var j = 0;
+            //Get current badge count
+            var not_count = $('.badge.bg-warning').html();            
             //append notifications
             for (var i = 0; i < rows.length; i++) {
-                $(".notifications").append("<li><a href='#'><span class='subject'><span class='from'>" + rows[i].field_agent_name + "</span><span class='time'>" + time_elapsed_string(rows[i].notification_created_at) + "</span></span><span class='message'>" + rows[i].message + "</span></a></li>");
+                $("ul.dropdown-menu.extended.inbox.notifications li:nth-child(1)").append("<li><a href='#'><span class='subject'><span class='from'>" + rows[i].field_agent_name + "</span><span class='time'>" + time_elapsed_string(rows[i].notification_created_at) + "</span></span><span class='message'>" + rows[i].message + "</span></a></li>");
                 j++;
             }
-            $(".notifications").append("<li><a href='#'>See all notifications</a></li>");
+            //Add new count to existing count
+            var final_count = parseInt(j) + parseInt(not_count);
             //Badge counter
-            $(".badge").html(j);
+            $(".badge.bg-warning").html(final_count);
         });
 
         socket.on('alerts', function (rows) {
-            $('.alerts').empty();
-            $(".alerts").append("<li><p>Alerts</p></li>");
             var j = 0;
-            //append alerts
+            //Get current badge count
+            var not_count = $('.badge.bg-success').html();            
+            //append notifications
             for (var i = 0; i < rows.length; i++) {
-                $(".alerts").append("<li><a href='#'><span class='subject'><span class='from'>" + rows[i].field_agent_name + "</span><span class='time'>" + time_elapsed_string(rows[i].notification_created_at) + "</span></span><span class='message'>" + rows[i].message + "</span></a></li>");
+                $("ul.dropdown-menu.extended.inbox.alerts li:nth-child(1)").append("<li><a href='#'><span class='subject'><span class='from'>" + rows[i].field_agent_name + "</span><span class='time'>" + time_elapsed_string(rows[i].notification_created_at) + "</span></span><span class='message'>" + rows[i].message + "</span></a></li>");
                 j++;
             }
-            $(".alerts").append("<li><a href='#'>See all alerts</a></li>");
+            //Add new count to existing count
+            var final_count = parseInt(j) + parseInt(not_count);
             //Badge counter
-            $(".badge").html(j);
+            $(".badge.bg-success").html(final_count);
+//            $('.alerts').empty();
+//            $(".alerts").append("<li><p>Alerts</p></li>");
+//            var j = 0;
+//            //append alerts
+//            for (var i = 0; i < rows.length; i++) {
+//                $(".alerts").append("<li><a href='#'><span class='subject'><span class='from'>" + rows[i].field_agent_name + "</span><span class='time'>" + time_elapsed_string(rows[i].notification_created_at) + "</span></span><span class='message'>" + rows[i].message + "</span></a></li>");
+//                j++;
+//            }
+//            $(".alerts").append("<li><a href='#'>See all alerts</a></li>");
+//            //Badge counter
+//            $(".badge").html(j);
         });
     }
 });	  
