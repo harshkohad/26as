38898467ac7cc252516afe893c7caf707f7e675e
harshkohad <harsh.kohad@gmail.com>
2018-05-14 19:03:14 +0530

    Update index.php

--- modules/applications/views/institutes/index.php
+++ modules/applications/views/institutes/index.php
@@ -23,7 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'filterModel' => $searchModel,
             'columns' => [
                 ['class' => 'yii\grid\SerialColumn'],
-                'id',
+                //'id',
                 'name',
                 //'download_pdf',
                 //'download_excel',
